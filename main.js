9c3d8d267fd421a18b9730ebf844156afd338aba
jingjing <jingjingwangacc@gmail.com>
2023-07-20 20:06:20 -0700

    weather

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     // const title = document.createElement('h1');
     // title.innerText = 'Online Chatroom';
-    document.querySelector('body').appendChild(title);
+    // document.querySelector('body').appendChild(title);
     // make AJAX call here....
 //     const newDiv = document.createElement('div')
 //     document.getElementById('container').appendChild(newDiv)
-    fetch('https://api.meteomatics.com/2023-07-20T18:22Z/t_min_2m_24h:C,t_max_2m_24h:C/postal_90042/json')
+    fetch('https://api.meteomatics.com/2023-07-20T18:22Z/t_min_2m_24h:C,t_max_2m_24h:C/postal_CH9014/json?access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJ2IjoxLCJ1c2VyIjoic2VsZi1lbXBsb3llZF93YW5nIiwiaXNzIjoibG9naW4ubWV0ZW9tYXRpY3MuY29tIiwiZXhwIjoxNjg5OTE1Njk2LCJzdWIiOiJhY2Nlc3MifQ.1hv7Y_tGqYv6DYgLqAbWfUOElJ1zK2b5dO-3MuTcSM6xkxKUrbKC6qMzCO2jUEtvNa7Us4g67eljHTDxgT77CQ')
     .then((data) => data.json())
     .then((data) => {
-        console.log(data);
+        console.log('data', data);
       })
       .catch((data) => console.log('alert'))
     });
